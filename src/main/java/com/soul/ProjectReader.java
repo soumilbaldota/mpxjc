8cfd0d5b30b3b3b892682473a866204752d8792b
Soumil Baldota <soumilbaldot@gmail.com>
2025-06-18 13:26:37 +0000

    refactor: rename the java library

--- src/main/java/com/soul/ProjectReader.java
+++ src/main/java/com/soul/ProjectReader.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-public class LibEnvMap {
+public class ProjectReader {
 
     private static byte[] resultBuffer = null;
 
